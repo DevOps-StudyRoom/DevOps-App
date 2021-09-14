208425889927e4f02f71530cbdd7adfa46a3794c
labyu <labyu2020@gmail.com>
2021-09-14 19:33:48 +0900

    로그 수집기 대시보드 초안 완성

--- src/main/java/site/haniumdevops/app/web/IndexController.java
+++ src/main/java/site/haniumdevops/app/web/IndexController.java
@@ -2,6 +2,7 @@ package site.haniumdevops.app.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -9,5 +10,10 @@ public class IndexController {
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/logging")
+    public String logging() {
+        return "logging";
     }
 }
